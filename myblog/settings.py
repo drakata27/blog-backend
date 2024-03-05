9ece20d2b27c8dcfe1721025b32ff21e92029bf2
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-03-05 14:35:37 +0000

    Bug fixes media

--- myblog/settings.py
+++ myblog/settings.py
@@ -140,7 +140,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
