0a4b399d0b9ad1c22fa0bb38d05cbe16a99ab0a8
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-03-06 15:21:19 +0000

    Changed CORS origin bug fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://blog-frontend-sigma-navy.vercel.app/",
+    "https://blog-frontend-sigma-navy.vercel.app",
 ]
 
 JAZZMIN_UI_TWEAKS = {
